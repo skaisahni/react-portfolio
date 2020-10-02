28d96fb6caabc83d938c38b79da256d4286c5d79
skai sahni <32442797+skaisahni@users.noreply.github.com>
2020-10-02 15:03:15 +0100

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -22,11 +22,6 @@ class Footer extends Component {
             <li>Modified with love by Skaisahni</li>
            </ul>
 
-           <ul className="copyright">
-              <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
-           </ul>
-
         </div>
         <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
      </div>
