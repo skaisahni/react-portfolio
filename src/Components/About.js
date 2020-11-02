c6d022928363cdf57b6c36edc34e67ae62e22f91
Sikander Nishad <skaicybertron@gmail.com>
2020-11-02 16:54:47 +0000

    sss

--- src/Components/About.js
+++ src/Components/About.js
@@ -31,9 +31,8 @@ class About extends Component {
                   <h2>Contact Details</h2>
                   <p className="address">
 						   <span>{name}</span><br />
-                         <span>{city}<br />
-						          {state},
-                   </span>
+						   <span> {city} {state}
+                   </span><br />
                      <span>{email}</span>
 					   </p>
                </div>
